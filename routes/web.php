76f8ca2127a7cc0b771b09161dd9ae679652f5d7
Joélson <joelsonjosue@hotmail.com>
2019-11-14 11:10:10 -0300

    Atualização

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,49 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function() {
+    return view('site.contact');
+});
+
+/*Route::middleware([])->group(function (){
+    Route::prefix('panel')->group(function (){
+        Route::namespace('Admin')->group(function (){
+            Route::name('panel.')->group(function (){
+
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+                Route::get('/financeiro', function (){
+                    return 'Financeiro Admin';
+                })->name('financeiro');
+                Route::get('/produtos', function (){
+                    return 'Produtos Admin';
+                })->name('produtos');
+                Route::get('/', function (){
+                    return redirect()->route('panel.dashboard');
+                })->name('home');
+
+            });
+
+        });
+    });
+});*/
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'panel',
+    'namespace' => 'Admin',
+    'as' => 'panel.'
+], function () {
+
+    Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+    Route::get('/financeiro', function (){
+        return 'Financeiro Admin';
+    })->name('financeiro');
+    Route::get('/produtos', function (){
+        return 'Produtos Admin';
+    })->name('produtos');
+    Route::get('/', function (){
+        return redirect()->route('panel.dashboard');
+    })->name('home');
+
+});
