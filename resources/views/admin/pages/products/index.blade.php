6af719a24cab052e50ce16418f43c26d6abecf12
Joélson <joelsonjosue@hotmail.com>
2019-11-14 17:42:39 -0300

    Atualização Final - 14/11/2019

---
+++ resources/views/admin/pages/products/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Gestão de Produtos')
+
+@section('content')
+    <h1>Exibindo os produtos {{ $teste }} {{ $opa }}</h1>    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('products', 'ProductController');
 
-Route::get('/', function () {
+Route::get('login', function (){
+    return 'Login';
+})->name('login');
+
+/*Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::put('products/{id}', 'ProductController@update')->name('products.update');
+Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('products/create', 'ProductController@create')->name('products.create');
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::post('products', 'ProductController@store')->name('products.store');*/
+
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/contato', function() {
     return view('site.contact');
-});
+});*/
 
 /*Route::middleware([])->group(function (){
     Route::prefix('panel')->group(function (){
